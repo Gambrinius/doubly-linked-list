43771c0a934d1a68b2c8c0fc9a4346f702be54d1
Ilya_Konon <ilya.konon.95@gmail.com>
2017-02-05 15:34:55 +0300

    added methods #constructor, #append, #head, #tail, #isEmpty, #clear

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,84 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
+    }
 
-    append(data) {}
+    // should add new data to the end of list
+    append(data) {
+        if (this.length === 0){ // empty list, add node to head of list.
+            let node = new Node(data); // prev = null, next = null
 
-    head() {}
+            this._tail = node;
+            this._head = node;
+            this.length ++;
+        } else { // list has elements
+            let node = new Node(data, this._tail, null); // prev = this._tail, next = null
 
-    tail() {}
+            this._tail.next = node; // add next pointer to previous node
+            this._tail = node;
+            this.length ++;
+        }
 
-    at(index) {}
+    }
 
-    insertAt(index, data) {}
+    // should return data from the this.head
+    head() {
+        if (this._head === null){
+            return this._head;
+        }else {
+            return this._head.data;
+        }
+    }
 
-    isEmpty() {}
+    // should return data from the this.tail
+    tail() {
+        if (this._tail === null){
+            return this._tail;
+        }else {
+            return this._tail.data;
+        }
+    }
 
-    clear() {}
+    // should return Node.data by index
+    at(index) {
 
-    deleteAt(index) {}
+    }
 
-    reverse() {}
+    // should insert data by index
+    insertAt(index, data) {
 
-    indexOf(data) {}
+    }
+
+    // should return true if list is empty
+    isEmpty() {
+        return this.length === 0;
+    }
+
+    // should clear the list
+    clear() {
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
+    }
+
+    // should delete element by index
+    deleteAt(index) {
+
+    }
+
+    // should reverse the list
+    reverse() {
+
+    }
+
+    // should return index of element if data is found, otherwise -1
+    indexOf(data) {
+
+    }
 }
 
 module.exports = LinkedList;
